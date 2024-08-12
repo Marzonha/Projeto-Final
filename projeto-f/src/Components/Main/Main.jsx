fe3529a257b516326e5953ded9448cf26c0b4513
Marlon <marzonhapacheco@gmail.com>
2024-08-12 11:26:38 -0300

    Por enquanto tá sem nada

--- projeto-f/src/Components/Main/Main.jsx
+++ projeto-f/src/Components/Main/Main.jsx
@@ -9,29 +9,7 @@ export default function Main() {
 
                 <h2 className={style.txtSecun}>Jogos em Promoções</h2>
 
-                <section className={style.sectionM}>
-                    <div className={style.containerS}>
-                        <div className={style.slider}>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-
-                        </div>
-
-                    </div>
+                <section>
 
                 </section>
             </main>
