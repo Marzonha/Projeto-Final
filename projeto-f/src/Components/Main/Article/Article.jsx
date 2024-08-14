398113e60f91ac3044c2cb12bb661f4b9ab56136
Marlon <marzonhapacheco@gmail.com>
2024-08-13 23:09:33 -0300

    Recriando da steam

--- projeto-f/src/Components/Main/Article/Article.jsx
+++ projeto-f/src/Components/Main/Article/Article.jsx
@@ -1,18 +1,19 @@
-import style from '../Article/article.module.css'
+import style from "../Article/article.module.css";
 
 export default function Article() {
-    return (
-        <>
-            {/* Article */}
-
-            <section className={style}>
-                <article>
-                    <h2>Título do Artigo</h2>
-                    <p>Texto do artigo</p>
-                    <small>Data de Publicação: 01/01/2022</small>
-                </article>
-            </section>
-
-        </>
-    )
+  return (
+    <>
+      {/* Article */}
+      <article className={style.Article}>
+        <section className={style.container}>
+          <div className={style.CardImgP}>imagem 1 P</div>
+          <div className={style.CardImgS1}>imagem 1 S</div>
+          <div className={style.CardImgS2}>imagem 1 S</div>
+          <div className={style.CardImgS3}>imagem 1 S</div>
+          <div className={style.CardImgS4}>imagem 1 S</div>
+          <div className={style.MCcard}></div>
+        </section>
+      </article>
+    </>
+  );
 }
