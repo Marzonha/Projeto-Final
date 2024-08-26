4ba1a8edd6f07c3bab2610e1adb5953e189f5906
Marlon <marzonhapacheco@gmail.com>
2024-08-26 17:36:52 -0300

    ajustes

--- projeto-f/src/Components/Main/X3d.jsx
+++ projeto-f/src/Components/Main/X3d.jsx
@@ -8,15 +8,16 @@ import '../Main/x3d.module.css';
 export default function X3d() {
     return (
         <>
-                <Canvas>
-                    <ambientLight intensity={0.5} />
-                    <OrbitControls />
-                    {/* <pointLight position={[10, 10, 10]} /> */}
-                    <Suspense fallback={null}>
-                        <XboxS/>
-                    </Suspense> 
-                    <Environment preset='sunset' />
-                </Canvas> 
+
         </>
     )
 }
+                // <Canvas>
+                //     <ambientLight intensity={0.5} />
+                //     <OrbitControls />
+                //     {/* <pointLight position={[10, 10, 10]} /> */}
+                //     <Suspense fallback={null}>
+                //         <XboxS/>
+                //     </Suspense> 
+                //     <Environment preset='sunset' />
+                // </Canvas> 
