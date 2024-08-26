fd1a537598e6f945a7cc9428ff4e4a1bfeb54ec5
Marlon <marzonhapacheco@gmail.com>
2024-08-26 17:21:24 -0300

    Ajustes

--- projeto-f/src/Components/Main/Main.jsx
+++ projeto-f/src/Components/Main/Main.jsx
@@ -2,12 +2,15 @@ import Header from "../Header/Header";
 import Article from "./Article/Article";
 
 import style from "../Main/main.module.css";
+import X3d from "./X3d";
+
 
 
 export default function Main() {
   return (
     <>
       <Header />
+
       <main>
         <h2>Jogos em Promoções</h2>
 
@@ -17,49 +20,46 @@ export default function Main() {
 
         <section>
           <div className={style.container2}>
-            <h1 className={style.titles}>Dia de jogs gratis</h1>
+            <h1 className={style.titles}>Dia de jogos gratis</h1>
           </div>
-          <div>
-            <div className={style.banner}>
-              <div className={style.slider} style={{ "--quantity": 10 }}>
-                <div className={style.model}></div>
-                <div className={style.item} style={{ "--position": 1 }}>
-                  <img src="./src/images/jogo1.avif" alt="" />
-                </div>
-                <div className={style.item} style={{ "--position": 2 }}>
-                  <img src="./src/images/jogo2.jpg" alt="" />
-                </div>
-                <div className={style.item} style={{ "--position": 3 }}>
-                  <img src="./src/images/jogo3.jpg" alt="" />
-                </div>
-                <div className={style.item} style={{ "--position": 4 }}>
-                  <img src="./src/images/jogo4.jpg" alt="" />
-                </div>
-                <div className={style.item} style={{ "--position": 5 }}>
-                  <img src="./src/images/jogo5.png" alt="" />
-                </div>
-                <div className={style.item} style={{ "--position": 6 }}>
-                  <img src="./src/images/jogo6.jpg" alt="" />
-                </div>
-                <div className={style.item} style={{ "--position": 7 }}>
-                  <img src="./src/images/jogo7.webp" alt="" />
-                </div>
-                <div className={style.item} style={{ "--position": 8 }}>
-                  <img src="./src/images/jogo8.jpg" alt="" />
-                </div>
-                <div className={style.item} style={{ "--position": 9 }}>
-                  <img src="./src/images/jogo9.jpg" alt="" />
-                </div>
-                <div className={style.item} style={{ "--position": 10 }}>
-                  <img src="./src/images/jogo10.jpg" alt="" />
-                </div>
-                <Canvas>
-                  <ambientLight />
-                  <Suspense></Suspense>
-                </Canvas>
+          <div className={style.banner}>
+            <div className={style.slider} style={{ "--quantity": 10 }}>
+              <div className={style.model}> <X3d/> </div>
+              <div className={style.item} style={{ "--position": 1 }}>
+                <img src="./src/images/jogo1.avif" alt="" />
+              </div>
+              <div className={style.item} style={{ "--position": 2 }}>
+                <img src="./src/images/jogo2.jpg" alt="" />
+              </div>
+              <div className={style.item} style={{ "--position": 3 }}>
+                <img src="./src/images/jogo3.jpg" alt="" />
+              </div>
+              <div className={style.item} style={{ "--position": 4 }}>
+                <img src="./src/images/jogo4.jpg" alt="" />
+              </div>
+              <div className={style.item} style={{ "--position": 5 }}>
+                <img src="./src/images/jogo5.png" alt="" />
+              </div>
+              <div className={style.item} style={{ "--position": 6 }}>
+                <img src="./src/images/jogo6.jpg" alt="" />
+              </div>
+              <div className={style.item} style={{ "--position": 7 }}>
+                <img src="./src/images/jogo7.webp" alt="" />
+              </div>
+              <div className={style.item} style={{ "--position": 8 }}>
+                <img src="./src/images/jogo8.jpg" alt="" />
+              </div>
+              <div className={style.item} style={{ "--position": 9 }}>
+                <img src="./src/images/jogo9.jpg" alt="" />
+              </div>
+              <div className={style.item} style={{ "--position": 10 }}>
+                <img src="./src/images/jogo10.jpg" alt="" />
               </div>
             </div>
           </div>
+        </section>
+
+        <section className={style.bg1}>
         </section>
       </main>
     </>
