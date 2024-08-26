fab11020e573dd2403bf01bf8e7e134e79cf5f6f
Marlon <marzonhapacheco@gmail.com>
2024-08-26 17:20:52 -0300

    Xbox 3D mudei o local dos arquivos e add um arquivo pra puxar o modelo

---
+++ projeto-f/src/Components/Main/X3d.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Canvas } from '@react-three/fiber';
+import { Environment, OrbitControls } from '@react-three/drei';
+
+import '../Main/x3d.module.css';
+
+export default function X3d() {
+    return (
+        <>
+                <Canvas>
+                    <ambientLight intensity={0.5} />
+                    <OrbitControls />
+                    {/* <pointLight position={[10, 10, 10]} /> */}
+                    <Suspense fallback={null}>
+                        <XboxS/>
+                    </Suspense> 
+                    <Environment preset='sunset' />
+                </Canvas> 
+        </>
+    )
+}
