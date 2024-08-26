3a186c257f89187c7598c6225e76fa11544986c1
Marlon <marzonhapacheco@gmail.com>
2024-08-25 23:24:50 -0300

    Deixar pra usar dps

--- projeto-f/src/Components/Main/Slider.jsx
+++ projeto-f/src/Components/Main/Slider.jsx
@@ -1,20 +1,19 @@
 // src/components/Slider.jsx
 
-import React from 'react';
+import React from "react";
 
 const Slider = () => {
   const games = [
-    '../images/jogo1.avif',
-    '../images/jogo2.jpg',
-    '../images/jogo3.jpg',
-    '../images/jogo4.jpg',
-    '../images/jogo5.png',
-    '../images/jogo6.jpg',
-    '../images/jogo7.webp',
-    '../images/jogo8.jpg',
-    '../images/jogo9.jpg',
-    '../images/jogo10.jpg',
-
+    "./src/images/jogo1.avif",
+    "./src/images/jogo2.jpg",
+    "./src/images/jogo3.jpg",
+    "./src/images/jogo4.jpg",
+    "./src/images/jogo5.png",
+    "./src/images/jogo6.jpg",
+    "./src/images/jogo7.webp",
+    "./src/images/jogo8.jpg",
+    "./src/images/jogo9.jpg",
+    "./src/images/jogo10.jpg",
 
     // ... adicione os caminhos para as outras imagens de jogos aqui
   ];
@@ -22,14 +21,18 @@ const Slider = () => {
   return (
     <section className="ImgM">
       <div className="banner">
-        <div className="slider" style={{ '--quantity': games.length }}>
-          {games.map((jogo, index) => (
-            <div className="item" key={index} style={{ '--position': index + 1 }}>
-              <img src={jogo} alt={`Jogo ${index + 1}`} />
-            </div>
-          ))}
+          <div className="slider" style={{ "--quantity": games.length }}>
+            {games.map((jogo, index) => (
+              <div
+                className="item"
+                key={index}
+                style={{ "--position": index + 1 }}
+              >
+                <img src={jogo} alt={`Jogo ${index + 1}`} />
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
     </section>
   );
 };
