4ab43bf93d93a703a2e5cbbd5e16ec9279b31936
Marlon <marzonhapacheco@gmail.com>
2024-08-25 23:24:31 -0300

    Slider refeito so faltando a imagem no meio

--- projeto-f/src/Components/Main/Main.jsx
+++ projeto-f/src/Components/Main/Main.jsx
@@ -1,90 +1,67 @@
-import Header from '../Header/Header'
-import Article from './Article/Article'
-import Slider from './Slider.jsx'
+import Header from "../Header/Header";
+import Article from "./Article/Article";
 
-import style from '../Main/main.module.css'
-
-import './slider/style.css'
-
-
+import style from "../Main/main.module.css";
 
 
 export default function Main() {
-    return (
-        <>
-            <Header />
-            <main className={style.containerSecun}>
-                <h2 className={style.txtSecun}>Jogos em Promoções</h2>
+  return (
+    <>
+      <Header />
+      <main>
+        <h2>Jogos em Promoções</h2>
 
-                <section>
-                    <Article />
-                </section>
+        <section>
+          <Article />
+        </section>
 
-                <section>
-                   <Slider/>             
-                </section>
-
-
-            </main>
-
-        </>
-    )
+        <section>
+          <div className={style.container2}>
+            <h1 className={style.titles}>Dia de jogs gratis</h1>
+          </div>
+          <div>
+            <div className={style.banner}>
+              <div className={style.slider} style={{ "--quantity": 10 }}>
+                <div className={style.model}></div>
+                <div className={style.item} style={{ "--position": 1 }}>
+                  <img src="./src/images/jogo1.avif" alt="" />
+                </div>
+                <div className={style.item} style={{ "--position": 2 }}>
+                  <img src="./src/images/jogo2.jpg" alt="" />
+                </div>
+                <div className={style.item} style={{ "--position": 3 }}>
+                  <img src="./src/images/jogo3.jpg" alt="" />
+                </div>
+                <div className={style.item} style={{ "--position": 4 }}>
+                  <img src="./src/images/jogo4.jpg" alt="" />
+                </div>
+                <div className={style.item} style={{ "--position": 5 }}>
+                  <img src="./src/images/jogo5.png" alt="" />
+                </div>
+                <div className={style.item} style={{ "--position": 6 }}>
+                  <img src="./src/images/jogo6.jpg" alt="" />
+                </div>
+                <div className={style.item} style={{ "--position": 7 }}>
+                  <img src="./src/images/jogo7.webp" alt="" />
+                </div>
+                <div className={style.item} style={{ "--position": 8 }}>
+                  <img src="./src/images/jogo8.jpg" alt="" />
+                </div>
+                <div className={style.item} style={{ "--position": 9 }}>
+                  <img src="./src/images/jogo9.jpg" alt="" />
+                </div>
+                <div className={style.item} style={{ "--position": 10 }}>
+                  <img src="./src/images/jogo10.jpg" alt="" />
+                </div>
+                <Canvas>
+                  <ambientLight />
+                  <Suspense></Suspense>
+                </Canvas>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
-
-
-
-
-{/* 
-                <section className={style.sectionM}>
-                    <div className={style.containerS}>
-                        <div className={style.slider}>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-                            <div className={style.item}>
-                                <img className={style.imgS} src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/10/df810159-3b33-417d-8158-cf1a185a0109-t4yhsaq1d168.png" alt="" /></div>
-
-                        </div>
-
-                    </div>
-
-                </section> */}               
-                
-                
-                
-                {/* <section className={style.Products}>
-                    <div className={style.container}>
-                        <h1 className={style.TitleHeading}>Recomendados</h1>
-                        <div className={style.Slider}>
-                            <button className={style.prevbtn}><img className={style.switchImg} src="./src/images/arrow-left.png" alt="" /></button>
-                            <button className={style.nextbtn}><img className={style.switchImg} src="./src/images/arrow-left.png" alt="" /></button>
-                            <div className={style.gamesSlider}>
-                                <div className={style.gamescard}>
-                                    <div className={style.productimage}>
-                                        <img className={style.imgGame} src="./src/images/jogo1.avif" alt="" />
-                                    </div>
-                                    <div>
-                                        <h2 className={style.txtC}> CoD Black Ops 2</h2>
-                                        <div className={style.containerStars} >
-                                            <img className={style.stars} src="./src/images/estrela.png" alt="" />
-                                            <img className={style.stars} src="./src/images/estrela.png" alt="" />
-                                            <img className={style.stars} src="./src/images/estrela.png" alt="" />
-                                            <img className={style.stars} src="./src/images/estrela.png" alt="" />
-                                            <img className={style.stars} src="./src/images/estrela.png" alt="" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
