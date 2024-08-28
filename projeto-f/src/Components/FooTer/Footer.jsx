b24de9f865727b3bc54f9e3ce1311855fb2d1d59
Marlon <marzonhapacheco@gmail.com>
2024-08-28 14:58:02 -0300

    Mudado o footer

--- projeto-f/src/Components/FooTer/Footer.jsx
+++ projeto-f/src/Components/FooTer/Footer.jsx
@@ -3,55 +3,57 @@ import style from "./footer.module.css";
 export default function Footer() {
   return (
     <>
-      <footer className={style.Footer}>
-        <section>
-          <div className={style.container}>
+ <footer>
+        <div className={style.containerfooter}>
+            <div className={style.rowfooter}>
+                <div className={style.footercol}>
+                    <h4>Empresa</h4>
+                    <ul>
+                        <li><a href="#"> Quem somos </a></li>
+                        <li><a href=""> nossos serviços </a></li>
+                        <li><a href=""> política de privacidade </a></li>
+                        <li><a href=""> programa de afiliados</a></li>
+                    </ul>
+                </div>
+                <div className={style.footercol}>
+                    <h4>Obter ajuda</h4>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Transporte</a></li>
+                        <li><a href="#">devoluções</a></li>
+                        <li><a href="#">Status De Pedido</a></li>
+                        <li><a href="#">Opções De Pagamento</a></li>
+                    </ul>
+                </div>
+                <div className={style.footercol}>
+                    <h4>Lojas de Inspiração</h4>
+                    <ul>
+                        <li><a href="#">Steam</a></li>
+                        <li><a href="#">Eneba</a></li>
+                        <li><a href="#">Epic Games</a></li>
+                        <li><a href="#">Microsft Game Pass</a></li>
+                    </ul>
+                </div>
+                <div className={style.footercol}>
+                    <h4>Se Cadastre</h4>
+                    <div className={style.formsub}>
+                        <form>
+                            <input type="e-mail" name="e-mail" placeholder="Email" autoComplete="e-mail"/>
+                          <button type="submit" value="e-mail">Cadastrar</button>
+                        </form>
+                    </div>
 
-            <div>
-              <h3>Contact</h3>
-              <p>Phone: +123 456 7890</p>
-              <p>Email: support@example.com</p>
+                    <div className={style.mediassocias}>
+                        <a href="#"> <i className={style.fafafacebook}></i> </a>
+                        <a href="#"> <i className={style.fafainstagram}></i> </a>
+                        <a href="#"> <i className={style.fafatwitter}></i> </a>
+                        <a href="#"> <i className={style.fafalinkedin}></i> </a>
+                    </div>
+
+                </div>
             </div>
-
-            <div className={style.RedesS}>
-              <h3>Social Media</h3>
-              <ul>
-                <li>
-                <img className={style.imgF} src="./src/images/facebook.png" alt="" />
-                  <a className={style.aF} href="#">Facebook</a>
-                </li>
-                <li>
-                <img className={style.imgF}  src="./src/images/" alt="" />
-                  <a className={style.aF}  href="#">Instagram</a>
-                </li>
-                <li>
-                <img className={style.imgF}  src="./src/images/twitter.png" alt="" />
-                  <a className={style.aF}  href="#">Twitter</a>
-                </li>
-                <li>
-                <img className={style.imgF}  src="./src/images/github.png.png" alt="" />
-                  <a className={style.aF}  href="#">Git Hub</a>
-                </li>
-                <li>
-                <img className={style.imgF}  src="./src/images/youtube.png" alt="" />
-                  <a className={style.aF}  href="#">YouTube</a>
-                </li>
-
-              </ul>
-            </div>
-
-            <div className={style.SobreNós}>
-              <h3>About Us</h3>
-              <p> 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                bibendum, ipsum eget condimentum semper, nunc urna varius velit,
-                ac rutrum metus eros eu justo.
-              </p>
-            </div>
-
-          </div>
-        </section>
-      </footer>
+        </div>
+    </footer>
     </>
   );
 
