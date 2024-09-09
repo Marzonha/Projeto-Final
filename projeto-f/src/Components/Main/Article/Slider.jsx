e802695f61a12206d9538da0b7e1a76ddafc3500
Marlon <marzonhapacheco@gmail.com>
2024-09-09 02:35:44 -0300

    criado um componente e ajustes Para que funcione o js, junto com ajustes na responsividade

--- projeto-f/src/Components/Main/Article/Slider.jsx
+++ projeto-f/src/Components/Main/Article/Slider.jsx
@@ -1,115 +1,260 @@
 import React, { useState } from "react";
-
-import style from './article.module.css'
+import style from "./article.module.css";
 
 const ImageSwitcher = () => {
-  const [imageSrc, setImageSrc] = useState("./src/images/cuphead.jpg ");
+  const [imageSrc, setImageSrc] = useState("./src/images/cuphead.jpg");
+  const [imageSrc2, setImageSrc2] = useState("./src/images/slider2P.jpg");
+  const [imageSrc3, setImageSrc3] = useState("./src/images/slider3P.avif");
+  const [imageSrc4, setImageSrc4] = useState("./src/images/slider4P.webp");
+  const [currentSlider, setCurrentSlider] = useState(1);
 
-// criar vários desses para as 4 imagens secundárias
-  const handleMouseOver = () => {
-    setImageSrc("./src/images/cena1.jpg");
+  const handleMouseOver = (src) => {
+    setImageSrc(src);
   };
-
-  const handleMouseOver2 = () => {
-    setImageSrc("./src/images/cena22.webp");
-    
-  };
-  
-  const handleMouseOver3 = () => {
-    setImageSrc("./src/images/cena3.webp");
-  };
-  
-  const handleMouseOver4 = () => {
-    setImageSrc("./src/images/cena4.webp");
-  };
-  
-// criar vários desses para as 4 imagens secundárias
-
 
   const handleMouseOut = () => {
     setImageSrc("./src/images/cuphead.jpg");
   };
 
+  const handleMouseOver2 = (src) => {
+    setImageSrc2(src);
+  };
+
+  const handleMouseOut2 = () => {
+    setImageSrc2("./src/images/slider2P.jpg");
+  };
+
+  const handleMouseOver3 = (src) => {
+    setImageSrc3(src);
+  };
+
+  const handleMouseOut3 = () => {
+    setImageSrc3("./src/images/slider3P.avif");
+  };
+
+  const handleMouseOver4 = (src) => {
+    setImageSrc4(src);
+  };
+
+  const handleMouseOut4 = () => {
+    setImageSrc4("./src/images/slider4P.webp");
+  };
+
+  const handlePrevSlider = () => {
+    setCurrentSlider(currentSlider === 1 ? 4 : currentSlider - 1);
+  };
+
+  const handleNextSlider = () => {
+    setCurrentSlider(currentSlider === 4 ? 1 : currentSlider + 1);
+  };
+
   return (
     <>
-    {/* Img comitado usado de referência */}
-
-      {/* <img
-      src={imageSrc}
-      alt="Imagem"
-      onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
-      style={{ width: '300px', height: '300px' }}
-    /> */}
-
-    {/* Img comitado usado de referência */}
-
       <article>
         <div className={style.slider}>
-          <div className={style.containerImg}>
-            {/* imagem principal */}
-            <img className={style.imgP} 
-            src={imageSrc} 
-            
-            alt="Imagem Card" />
-            
-            {/* imagem principal */}
+          {currentSlider === 1 && (
+            <div className={style.containerImg}>
+              <img className={style.imgP} src={imageSrc} alt="Imagem Card" />
 
-            <div className={style.ImgS}>
-
-              {/* imagens secundárias */}
-
-              <img className={style.imgS} 
-              src="./src/images/cena1.jpg" 
-              alt="Imagem Card" 
-              onMouseOver={handleMouseOver}
-              onMouseOut={handleMouseOut}
-              
-
-
-              />
-
-              <img className={style.imgS} 
-              src="./src/images/cena22.webp" 
-              alt="Imagem Card" 
-              onMouseOver={handleMouseOver2}
-              onMouseOut={handleMouseOut}
-             
-              />
-
-              <img
-                className={style.imgS}
-                src="./src/images/cena3.webp"
-                alt="Imagem Card"
-                onMouseOver={handleMouseOver3}
-                onMouseOut={handleMouseOut}
-
-              />
-
-              <img
-                className={style.imgS}
-                src="./src/images/cena4.webp"
-                alt="Imagem Card"
-                onMouseOver={handleMouseOver4}
-                onMouseOut={handleMouseOut}
-              />
-              {/* imagens secundárias */}
+              <div className={style.ImgS}>
+                <div>
+                  <img
+                    className={style.imgS}
+                    src="./src/images/cena1.jpg"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver("./src/images/cena1.jpg")
+                    }
+                    onMouseOut={handleMouseOut}
+                  />
+                  <img
+                    className={style.imgS}
+                    src="./src/images/cena22.webp"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver("./src/images/cena22.webp")
+                    }
+                    onMouseOut={handleMouseOut}
+                  />
+                </div>
+                <div>
+                  <img
+                    className={style.imgS}
+                    src="./src/images/cena3.webp"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver("./src/images/cena3.webp")
+                    }
+                    onMouseOut={handleMouseOut}
+                  />
+                  <img
+                    className={style.imgS}
+                    src="./src/images/cena4.webp"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver("./src/images/cena4.webp")
+                    }
+                    onMouseOut={handleMouseOut}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
+          )}
+
+          {currentSlider === 2 && (
+            <div className={style.containerImg}>
+              <img className={style.imgP} src={imageSrc2} alt="Imagem Card" />
+
+              <div className={style.ImgS}>
+                <div>
+                  <img
+                    className={style.imgS}
+                    src="./src/images/slider2_1.jpg"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver2("./src/images/slider2_1.jpg")
+                    }
+                    onMouseOut={handleMouseOut2}
+                  />
+                  <img
+                    className={style.imgS}
+                    src="./src/images/slider2_2.avif"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver2("./src/images/slider2_2.avif")
+                    }
+                    onMouseOut={handleMouseOut2}
+                  />
+                </div>
+                <div>
+                  <img
+                    className={style.imgS}
+                    src="./src/images/slider2_3.webp"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver2("./src/images/slider2_3.webp")
+                    }
+                    onMouseOut={handleMouseOut2}
+                  />
+                  <img
+                    className={style.imgS}
+                    src="./src/images/slider2_4.webp"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver2("./src/images/slider2_4.webp")
+                    }
+                    onMouseOut={handleMouseOut2}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+
+          {currentSlider === 3 && (
+            <div className={style.containerImg}>
+              <img className={style.imgP} src={imageSrc3} alt="Imagem Card" />
+
+              <div className={style.ImgS}>
+                <div>
+                  <img
+                    className={style.imgS}
+                    src="./src/images/slider3_1.webp"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver3("./src/images/slider3_1.webp")
+                    }
+                    onMouseOut={handleMouseOut3}
+                  />
+                  <img
+                    className={style.imgS}
+                    src="./src/images/slider3_2.webp"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver3("./src/images/slider3_2.webp")
+                    }
+                    onMouseOut={handleMouseOut3}
+                  />
+                </div>
+                <div>
+                  <img
+                    className={style.imgS}
+                    src="./src/images/slider3_3.webp"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver3("./src/images/slider3_3.webp")
+                    }
+                    onMouseOut={handleMouseOut3}
+                  />
+                  <img
+                    className={style.imgS}
+                    src="./src/images/slider3_4.webp"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver3("./src/images/slider3_4.webp")
+                    }
+                    onMouseOut={handleMouseOut3}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+
+          {currentSlider === 4 && (
+            <div className={style.containerImg}>
+              <img className={style.imgP} src={imageSrc4} alt="Imagem Card" />
+
+              <div className={style.ImgS}>
+                <div>
+                  <img
+                    className={style.imgS}
+                    src="./src/images/slider4_1.webp"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver4("./src/images/slider4_1.webp")
+                    }
+                    onMouseOut={handleMouseOut4}
+                  />
+                  <img
+                    className={style.imgS}
+                    src="./src/images/slider4_2.webp"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver4("./src/images/slider4_2.webp")
+                    }
+                    onMouseOut={handleMouseOut4}
+                  />
+                </div>
+                <div>
+                  <img
+                    className={style.imgS}
+                    src="./src/images/slider4_3.webp"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver4("./src/images/slider4_3.webp")
+                    }
+                    onMouseOut={handleMouseOut4}
+                  />
+                  <img
+                    className={style.imgS}
+                    src="./src/images/slider4_4.webp"
+                    alt="Imagem Card"
+                    onMouseOver={() =>
+                      handleMouseOver4("./src/images/slider4_4.webp")
+                    }
+                    onMouseOut={handleMouseOut4}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
         </div>
 
         <div className={style.btnCr}>
-          <button className={style.btnac}>
-            <a href="#"></a>
+          <button className={style.btnac} onClick={handlePrevSlider}>
+            Anterior
           </button>
-          <button className={style.btnac}>
-            <a href="#"></a>
-          </button>
-          <button className={style.btnac}>
-            <a href="#"></a>
-          </button>
-          <button className={style.btnac}>
-            <a href="#"></a>
+          <button className={style.btnac} onClick={handleNextSlider}>
+            Próximo
           </button>
         </div>
       </article>
