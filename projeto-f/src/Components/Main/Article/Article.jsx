db4c78084a3d2e1f1efb60e767c12af60e2db435
Marlon <marzonhapacheco@gmail.com>
2024-09-08 23:19:39 -0300

    ajustes ajustes no css e no article

--- projeto-f/src/Components/Main/Article/Article.jsx
+++ projeto-f/src/Components/Main/Article/Article.jsx
@@ -1,36 +1,13 @@
 import style from "../Article/article.module.css";
 
-
+import Slider from './Slider'
 
 export default function Article() {
   return (
     <>
 
-      <article>
-        <div className={style.slider}>
-          <div className={style.containerImg} >
-            <img className={style.imgP} src="./src/images/cuphead.jpg" alt="" />
-            <div className={style.ImgS}>
-              <img className={style.imgS} src="./src/images/cena1.jpg" alt="" />
-
-              <img className={style.imgS} src="./src/images/cena2.jpg" alt="" />
-
-              <img className={style.imgS} src="./src/images/cena3.webp" alt="" />
-
-              <img className={style.imgS} src="./src/images/cena4.webp" alt="" />
-
-            </div>
-          </div>
-        </div>
-
-        <div className={style.btnCr}>
-          <button className={style.btnac}><a href="#"></a></button>
-          <button className={style.btnac}><a href="#"></a></button>
-          <button className={style.btnac}><a href="#"></a></button>
-          <button className={style.btnac}><a href="#"></a></button>
-        </div>
-
-      </article>
+      <Slider/>
+     
 
     </>
   );
